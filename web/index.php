cc4b74f9a064f720b751558d4368c8259206e6e5
Hawk <ikonen.4rt@gmail.com>
2017-09-15 21:07:31 +0300

    CSS Added
    style.css added and index.page modified accordingly.

--- web/index.php
+++ web/index.php
@@ -8,6 +8,7 @@
   <meta name="description" content="KotH manager">
   <meta name="author" content="Panu 'Pasketi' Siitonen">
   <script src='https://www.google.com/recaptcha/api.js'></script>
+  <style><?php include "style.css";?></style>
 <?php
 	if ($_POST != null) {
 		if ($_POST["passwd"] == $_POST["passwdCheck"]) {
@@ -35,14 +36,26 @@
   <br />
   <h3>Create a new tournament</h3>
   <form action="index.php" method="post">
-    Tournament name *: <input type="text" name="newTournament" maxlength="20" required><br />
-    Your name *: <input type="text" name="newOwner" maxlength="20" required><br />
-    Your email *: <input type="email" name="newOwnerEmail" required><br /><br />
-	You will find your tournament with an ID. If you leave the tournament ID empty, you will get a generated, unique 10 digit ID.<br />
-    Your personal tournament ID: <input type="text" name="ownId" maxlength="20"><br /><br />
-	You will use this password to manage your tournament. Share it only with your moderators!<br />
-    Your tournament password *: <input type="password" name="passwd" required><br />
-	Your tournament password again *: <input type="password" name="passwdCheck" required><br />
+    <label for="Tournament">
+	<span>Tournament name *:</span> <input type="text" name="newTournament" maxlength="20" required>
+    </label>
+	<label for="Name">
+	<span>Your name *:</span> <input type="text" name="newOwner" maxlength="20" required>
+    </label>
+	<label for="Email">
+	<span>Your email *:</span> <input type="email" name="newOwnerEmail" required>
+    </label>
+	<label for="TournamentID">
+	<span>Your personal tournament ID:</span> <input type="text" name="ownId" maxlength="20">
+	<sub class="subtext">You will find your tournament with an ID. If you leave the tournament<br/>ID empty, you will get a generated, unique 10 digit ID.</sub>
+	</label>
+	<label for="Password">
+	<span>Your tournament password *:</span> <input type="password" name="passwd" required>
+	<sub class="subtext">You will use this password to manage your tournament.<br/>Share it only with your moderators!</sub>
+	</label>
+	<label for ="PasswordAgain">
+	<span>Your tournament password again *:</span> <input type="password" name="passwdCheck" required>
+	</label>
 	<p>Fields marked with a * are required</p>
     <div class="g-recaptcha" data-sitekey="6LeHxi8UAAAAAL7017VTiam5iT8TJ47Tl9leOEnn"></div>
     <input type="submit">
