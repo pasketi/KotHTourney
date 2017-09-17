06b06e6d175b851c33ea321281d832b38192fce9
pasketi <pasketi@gmail.com>
2017-09-18 00:52:41 +0300

    Longer inactivity window

--- web/admin.php
+++ web/admin.php
@@ -6,7 +6,7 @@
 	
 	session_start();
 	
-	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 60)) {
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600)) {
 		// last request was more than 30 minutes ago
 		session_unset();     // unset $_SESSION variable for the run-time 
 		session_destroy();   // destroy session data in storage
